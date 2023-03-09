33cd291be6d2967e5809e70317c0f24d0b4e8153
Michael Peter Gower Amiethyst <designatevoid@gmail.com>
2023-03-09 15:27:26 -0800

    Can now process multiple lines

--- src/main/java/com/bashpile/ArrayUtils.java
+++ src/main/java/com/bashpile/ArrayUtils.java
@@ -2,7 +2,8 @@ package com.bashpile;
 
 public class ArrayUtils extends org.apache.commons.lang3.ArrayUtils {
 
-    public static String[] arrayOf(String... str) {
+    @SafeVarargs
+    public static <T> T[] of(T... str) {
         return str;
     }
 }
--- src/main/java/com/bashpile/renderers/WslBashRenderer.java
+++ src/main/java/com/bashpile/renderers/WslBashRenderer.java
@@ -1,10 +1,11 @@
 package com.bashpile.renderers;
 
+import com.bashpile.ArrayUtils;
 import com.bashpile.AstNode;
 
 /** For Windows Subsystem for Linux Bash 5.1.16 with Ubuntu */
-public class WslBashRenderer implements ShellScriptRenderer<Integer> {
-    public Integer render(AstNode<Integer> root) {
-        return root.getValue();
+public class WslBashRenderer implements ShellScriptRenderer<Integer[]> {
+    public Integer[] render(AstNode<Integer[]> root) {
+        return ArrayUtils.of(root.getValue());
     }
 }
