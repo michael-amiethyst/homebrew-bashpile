6e5322d4b42f2741273db30741c0d0d7a7e28863
michael-amiethyst <michaelamiethyst@gmail.com>
2023-08-26 09:31:26 -0700

    Moved AntlrUtils methods mostly to BashTranslationHelper.  Two to BashpileMain

--- Formula/bashpile.rb
+++ Formula/bashpile.rb
@@ -1,7 +1,7 @@
 class Bashpile < Formula
   desc "Bashpile - The Bash Transpiler: Write in a modern language and run in a Bash5 shell!"
   homepage "https://github.com/michael-amiethyst/homebrew-bashpile"
-  url "https://github.com/michael-amiethyst/homebrew-bashpile", using: :git, branch: "main", tag: "0.10.1"
+  url "https://github.com/michael-amiethyst/homebrew-bashpile", using: :git, branch: "main", tag: "0.11.0"
   head "https://github.com/michael-amiethyst/homebrew-bashpile", using: :git, branch: "feature/conditionals"
   license "MIT"
 
