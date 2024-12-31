d42b32af5d12b51c1bac7d4b5b1c79c3e3f74edf
Michael Amiethyst <michaelamiethyst@gmail.com>
2024-12-31 17:05:12 -0500

    0.25.0 - Updated head to development

--- Formula/bashpile.rb
+++ Formula/bashpile.rb
@@ -3,7 +3,7 @@ class Bashpile < Formula
   homepage "https://github.com/michael-amiethyst/homebrew-bashpile"
   license "MIT"
   url "https://github.com/michael-amiethyst/homebrew-bashpile", using: :git, branch: "main", tag: "0.25.0"
-  head "https://github.com/michael-amiethyst/homebrew-bashpile", using: :git, branch: "feature/bast"
+  head "https://github.com/michael-amiethyst/homebrew-bashpile", using: :git, branch: "development"
 
   # foundational dependencies
   depends_on "openjdk"
